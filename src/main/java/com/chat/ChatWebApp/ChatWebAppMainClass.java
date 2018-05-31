19238405b3d725e723ee6c76506833113afd9099
popoparo <stas.hazhevskiy@gmail.com>
2018-05-31 19:49:15 +0300

    The project has been created!

---
+++ src/main/java/com/chat/ChatWebApp/ChatWebAppMainClass.java
@@ -0,0 +1,12 @@
+package com.chat.ChatWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatWebAppMainClass {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatWebAppMainClass.class, args);
+	}
+}
