f4410b7c929fae13fc5323b5605d47d065118eb6
Vitaliy <lol.vasya.lol.kreash@gmail.com>
2018-06-05 00:50:44 +0300

    Added main class

---
+++ src/main/java/com/web/app/chat/MainClass.java
@@ -0,0 +1,12 @@
+package com.web.app.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainClass {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainClass.class, args);
+	}
+}
